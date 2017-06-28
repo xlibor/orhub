91dd893b43b22fda3158f113a20ac3dbbc1a1b52
xlibor <xlibor@163.com>
2017-06-28 22:26:37 +0800

    update

---
+++ app/http/context.lua
@@ -0,0 +1,20 @@
+
+local _M = {
+	_cls_ = '',
+	_ext_ = {
+		path = 'lxlib.http.context',
+	}
+}
+
+function _M:init()
+
+end
+
+function _M:_get_(key)
+
+	return self:__super('_get_', key)
+end
+
+
+return _M
+
