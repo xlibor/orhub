a81956eabf18167d060c8d157f7d9a0a4c6c04d1
xlibor <xlibor@163.com>
2018-01-08 10:11:40 +0800

    updates

--- db/shift/2016_05_09_154236_create_tags_table.lua
+++ db/shift/2016_05_09_154236_create_tags_table.lua
@@ -10,14 +10,11 @@ function _M:up(schema)
 
     schema:create(app:conf('taggable.tags_table_name'), function(table)
         table:increments('id')
-        table:string('slug', 255):unique()
-        table:string('name', 255):unique()
+        table:string('slug'):unique()
+        table:string('name'):unique()
         table:text('description'):nullable()
         table:boolean('suggest'):default(false)
         table:integer('count'):unsigned():default(0)
-        -- count of how many times this tag was used
-        -- For: Baum Nested Set
-        -- See: https://github.com/etrepat/baum#migration-configuration
         table:integer('parent_id'):nullable()
         table:integer('lft'):nullable()
         table:integer('rgt'):nullable()
@@ -31,7 +28,7 @@ function _M:up(schema)
         else 
             table:integer('taggable_id'):unsigned():index()
         end
-        table:string('taggable_type', 255):index()
+        table:string('taggable_type'):index()
         table:integer('tag_id'):unsigned():index()
         table:foreign('tag_id'):references('id'):on(app:conf('taggable.tags_table_name')):onDelete('cascade')
     end)
